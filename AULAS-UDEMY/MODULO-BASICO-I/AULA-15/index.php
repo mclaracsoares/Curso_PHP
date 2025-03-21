a48a9c5235e7d38af99266ea63e674d88246c147
mclaracsoares <maria.00000849097@unicap.br>
2025-03-21 10:12:23 -0300

    Organização das pastas do curso

---
+++ AULAS-UDEMY/MODULO-BASICO-I/AULA-15/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// @include, require
+
+// @include vai incluir algum arquivo quer que ele exista ou nao 
+// o require vai ser obrigatorio que o arquivo exista, senao ele vai dar erro fatal no códig
+
+// formato:
+// @include "arquivo.php";
+// include "arquivo.php";
+
+// require "arquivo.php";
+
+
+// require_once e include_once
+
+// ele garante que no codigo a gente só importe o arquivo uma vez
---
+++ AULAS-UDEMY/MODULO-BASICO-I/AULA-16/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// funções
+
+function adicao($a, $b){
+    return $a + $b;
+}
+
+$x = adicao(1, 2);
+$y = adicao($x, 5);
+
+
+// obs: o escopo da funcao so funciona dentro da funcao
